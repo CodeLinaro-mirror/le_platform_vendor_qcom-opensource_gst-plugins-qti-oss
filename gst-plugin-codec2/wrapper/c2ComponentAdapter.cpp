d8f8337565689c7e2a738924d83565c06e83389f
Mi Zhou <quic_mizhou@quicinc.com>
2022-01-14 18:49:05 +0800

    qticodec2: fix queue buffer hang-on issue
    If pending works reach maximums, waitForProgressOrStateChange
    will wait and no more buffer to be queued to component.
    
    Refer to IComponentListener.hal#onInputBuffersReleased on LA,
    try 30ms timeout-wait to wake up queue_nb again so as to ease pending works,
    e.g., when the component needs more input before an output can be produced.
    
    Back to block-wait once a new onWorkDone arrival from C2.
    
    Change-Id: Ibaf930fe2ce0ce3e991e0bc1e8ef72ff992d0588
    Signed-off-by: Mi Zhou <quic_mizhou@quicinc.com>

--- gst-plugin-codec2/wrapper/c2ComponentAdapter.cpp
+++ gst-plugin-codec2/wrapper/c2ComponentAdapter.cpp
@@ -70,6 +70,7 @@ C2ComponentAdapter::C2ComponentAdapter(std::shared_ptr<C2Component> comp)
     mGraphicPool = nullptr;
     mMapBufferToCpu = false;
     mNumPendingWorks = 0;
+    mPendingTimeOut = 0;
 }
 
 C2ComponentAdapter::~C2ComponentAdapter()
@@ -262,11 +263,11 @@ c2_status_t C2ComponentAdapter::prepareC2Buffer(std::shared_ptr<C2Buffer>* c2Buf
 }
 
 c2_status_t C2ComponentAdapter::waitForProgressOrStateChange(
-    uint32_t maxPendingWorks,
-    uint32_t timeoutMs)
+    uint32_t maxPendingWorks)
 {
 
     std::unique_lock<std::mutex> ul(mLock);
+    uint32_t timeoutMs = mPendingTimeOut;
     LOG_MESSAGE("waitForProgressOrStateChange: pending = %u", mNumPendingWorks);
 
     while (mNumPendingWorks > maxPendingWorks) {
@@ -274,6 +275,7 @@ c2_status_t C2ComponentAdapter::waitForProgressOrStateChange(
             if (mCondition.wait_for(ul, timeoutMs * 1ms) == std::cv_status::timeout) {
                 LOG_ERROR("Timed-out waiting for work / state-transition (pending=%u)",
                     mNumPendingWorks);
+                mNumPendingWorks--;
                 return C2_TIMED_OUT;
             } else {
                 LOG_MESSAGE("wait done");
@@ -282,6 +284,17 @@ c2_status_t C2ComponentAdapter::waitForProgressOrStateChange(
         } else if (timeoutMs == 0) {
             mCondition.wait(ul);
         }
+    }
+
+    /* In case notification is done immediately by ease pending,
+     * but it's meant to wait for another round by mPendingTimeOut ms.
+     * So continue to wait once more.
+     */
+    if (mPendingTimeOut > 0) {
+        timeoutMs = mPendingTimeOut;
+        LOG_WARNING("wait another round in case of timeout changed to %d ms",
+            timeoutMs);
+        mCondition.wait_for(ul, timeoutMs * 1ms);
     }
 
     return C2_OK;
@@ -417,7 +430,7 @@ c2_status_t C2ComponentAdapter::queue(BufferDescriptor* buffer)
     workList.push_back(std::move(work));
 
     if (!isEOSFrame) {
-        waitForProgressOrStateChange(MAX_PENDING_WORK, 0);
+        waitForProgressOrStateChange(MAX_PENDING_WORK);
     } else {
         LOG_MESSAGE("EOS reached");
     }
@@ -586,6 +599,7 @@ void C2ComponentAdapter::handleWorkDone(
         uint64_t bufferIdx = 0;
         C2FrameData::flags_t outputFrameFlag = worklet->output.flags;
         uint64_t timestamp = worklet->output.ordinal.timestamp.peeku();
+        bool ease_pending = false;
 
         while (!worklet->output.configUpdate.empty()) {
             std::unique_ptr<C2Param> param;
@@ -605,6 +619,7 @@ void C2ComponentAdapter::handleWorkDone(
                             LOG_ERROR("Failed to get allocator");
                             break;
                         }
+                        ease_pending = true;
                         std::shared_ptr<android::C2AllocatorGBM> allocatorGBM = std::dynamic_pointer_cast<android::C2AllocatorGBM>(allocator);
                         allocatorGBM->setMaxAllocationCount(outputDelay.value);
                     }
@@ -634,6 +649,7 @@ void C2ComponentAdapter::handleWorkDone(
             mCallback->onOutputBufferAvailable(buffer, bufferIdx, timestamp, outputFrameFlag);
             std::unique_lock<std::mutex> ul(mLock);
             mNumPendingWorks--;
+            mPendingTimeOut = 0;
             mCondition.notify_one();
         } else {
 
@@ -644,12 +660,33 @@ void C2ComponentAdapter::handleWorkDone(
                 LOG_MESSAGE("Component(%p) work incomplete, means an input frame results in multiple"
                             "output frames, or codec config update event",
                     this);
-                continue;
+                /* If pending works reach maximums, waitForProgressOrStateChange
+                 * will wait and no more buffer to be queued to component.
+                 * In some cases component configure updates, output buffers
+                 * will be re-allocated in C2 and it needs more input for output.
+                 *
+                 * Refer to IComponentListener.hal#onInputBuffersReleased on LA,
+                 * try timeout-wait to wake up queue_nb again so as to ease pending works,
+                 * e.g., when the component needs more input before an output can be produced.
+                 *
+                 * Back to block-wait once a new onWorkDone arrival from C2.
+                 */
+                if (ease_pending) {
+                    LOG_MESSAGE("Component(%p) ease pending works", this);
+                    std::unique_lock<std::mutex> ul(mLock);
+                    mNumPendingWorks--;
+                    mPendingTimeOut = 30; // try to wait for 30ms timeout
+                    mCondition.notify_one();
+                    break;
+                } else {
+                    continue;
+                }
             } else {
                 LOG_ERROR("Incorrect number of output buffers: %lu", worklet->output.buffers.size());
             }
             std::unique_lock<std::mutex> ul(mLock);
             mNumPendingWorks--;
+            mPendingTimeOut = 0;
             mCondition.notify_one();
             break;
         }
