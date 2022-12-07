e9085e5532538ce002e5aa767e6cdb6a62bac3d4
Shiming Dong <quic_shimdong@quicinc.com>
2022-12-07 15:48:19 +0800

    gstvesdeliver: add a secure property to specify the secure mode
    Add support to allocate secure/non-secure ion buffer and
    share it to dec plugin
    
    for example:
    gst-launch-1.0 filesrc location=xxx.mp4 ! qtdemux ! h264parse !
    vesdeliver secure=1 ! qcodec2h264dec secure=1 ! waylandsink
    
    Change-Id: I33399d0138d26ccc603d96162761d5e05f0b4f40
    Signed-off-by: Shiming Dong <quic_shimdong@quicinc.com>

--- gst-plugin-vesdeliver/gstvesdeliver.c
+++ gst-plugin-vesdeliver/gstvesdeliver.c
@@ -9,7 +9,7 @@
  * <title>Example launch line</title>
  * |[
  * gst-launch filesrc location=xxx.mp4 ! qtdemux ! h264parse !
- * vesdeliver ! qcodec2h264dec ! queue ! waylandsink
+ * vesdeliver secure=1 ! qcodec2h264dec secure=1 ! waylandsink
  * ]|
  * </refsect2>
  */
@@ -18,27 +18,42 @@
 #include "config.h"
 #endif
 
+#include <stdio.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <dlfcn.h>
 #include "gstvesdeliver.h"
+#include "gstvesdeliverpool.h"
 
-GST_DEBUG_CATEGORY_STATIC (vesdeliver_debug);
+/* Dynamically load libs by dlopen. */
+static const char *crypto_lib_name  = "libcontentcopy.so";
+static const char *crypto_app_name = "smpcpyap64";
+
+GST_DEBUG_CATEGORY (vesdeliver_debug);
 #define GST_CAT_DEFAULT vesdeliver_debug
 
-#define DEFAULT_PROP_MIN_BUFFERS      6
-#define DEFAULT_PROP_MAX_BUFFERS      16
+enum {
+  PROP_0,
+  PROP_SECURE,
+};
 
 static GstStaticPadTemplate sink_tmpl = GST_STATIC_PAD_TEMPLATE ("sink",
     GST_PAD_SINK,
     GST_PAD_ALWAYS,
-    GST_STATIC_CAPS ("ANY"));
+    GST_STATIC_CAPS (H264_CAPS";"H265_CAPS";"VP9_CAPS";"MPEG2_CAPS));
 
 static GstStaticPadTemplate src_tmpl = GST_STATIC_PAD_TEMPLATE ("src",
     GST_PAD_SRC,
-    GST_PAD_SOMETIMES,
-    GST_STATIC_CAPS ("ANY"));
+    GST_PAD_ALWAYS,
+    GST_STATIC_CAPS (H264_CAPS";"H265_CAPS";"VP9_CAPS";"MPEG2_CAPS));
 
 #define gst_vesdeliver_parent_class parent_class
-G_DEFINE_TYPE (GstVesDeliver, gst_vesdeliver,
-               GST_TYPE_BASE_TRANSFORM);
+G_DEFINE_TYPE (GstVesDeliver, gst_vesdeliver, GST_TYPE_BASE_TRANSFORM);
+
+#define DEFAULT_SECURE_MODE    (TRUE)
+#define SECURE_COPY_RETURN_SUCCESS      0
+#define SECURE_COPY_NONSECURE_TO_SECURE 0
 
 static GstFlowReturn
 gst_vesdeliver_prepare_output_buffer (GstBaseTransform * base,
@@ -46,11 +61,18 @@ gst_vesdeliver_prepare_output_buffer (GstBaseTransform * base,
 static GstFlowReturn
 gst_vesdeliver_transform (GstBaseTransform * trans, GstBuffer * inbuffer,
     GstBuffer * outbuffer);
+static void
+gst_vesdeliver_set_property(GObject* object, guint property_id,
+    const GValue* value, GParamSpec* pspec);
+static void
+gst_vesdeliver_get_property(GObject* object, guint property_id,
+    GValue* value, GParamSpec* pspec);
+static void
+gst_vesdeliver_finalize (GObject * object);
 static gboolean
-gst_vesdeliver_decide_allocation (GstBaseTransform * base, GstQuery * query);
-/* state change functions */
-static GstStateChangeReturn
-gst_vesdeliver_change_state (GstElement * element, GstStateChange transition);
+gst_vesdeliver_start (GstBaseTransform* trans);
+static gboolean
+gst_vesdeliver_stop (GstBaseTransform* trans);
 
 static void
 gst_vesdeliver_class_init (GstVesDeliverClass * klass)
@@ -63,13 +85,25 @@ gst_vesdeliver_class_init (GstVesDeliverClass * klass)
   gstelement_class = (GstElementClass *) klass;
   gstbasetrans_class = GST_BASE_TRANSFORM_CLASS (klass);
 
-  gstelement_class->change_state = GST_DEBUG_FUNCPTR (gst_vesdeliver_change_state);
+  gobject_class->set_property = gst_vesdeliver_set_property;
+  gobject_class->get_property = gst_vesdeliver_get_property;
+  gobject_class->finalize = gst_vesdeliver_finalize;
+
+  g_object_class_install_property(
+      gobject_class,
+      PROP_SECURE,
+      g_param_spec_boolean(
+          "secure",
+          "Secure",
+          "video pipeline in secure mode, will allocate secure buffer",
+          DEFAULT_SECURE_MODE,
+          (GParamFlags)(G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS)));
+
   gstbasetrans_class->transform = GST_DEBUG_FUNCPTR (gst_vesdeliver_transform);
   gstbasetrans_class->prepare_output_buffer =
       GST_DEBUG_FUNCPTR (gst_vesdeliver_prepare_output_buffer);
-
-//  gstbasetrans_class->decide_allocation =
-//      GST_DEBUG_FUNCPTR (gst_vesdeliver_decide_allocation);
+  gstbasetrans_class->start = GST_DEBUG_FUNCPTR (gst_vesdeliver_start);
+  gstbasetrans_class->stop = GST_DEBUG_FUNCPTR (gst_vesdeliver_stop);
 
   gst_element_class_add_static_pad_template (gstelement_class, &sink_tmpl);
   gst_element_class_add_static_pad_template (gstelement_class, &src_tmpl);
@@ -77,124 +111,106 @@ gst_vesdeliver_class_init (GstVesDeliverClass * klass)
   gst_element_class_set_static_metadata (gstelement_class,
       "QTI Video Element Stream Deliver",
       "Deliver/Video",
-      "video deliver plugin which supports secure buffer sharing", "QTI");
+      "video deliver plugin which supports secure buffer sharing with dec plugin",
+      "QTI");
 }
 
 static void
 gst_vesdeliver_init (GstVesDeliver * vesdeliver)
 {
-  gst_base_transform_set_in_place (GST_BASE_TRANSFORM (vesdeliver), FALSE);
+  vesdeliver->secure = TRUE;
+  vesdeliver->allocator = NULL;
+  vesdeliver->secure_handle = NULL;
 
-  vesdeliver->outpool = NULL;
+  vesdeliver->crypto_handle = dlopen (crypto_lib_name, RTLD_NOW);
+  if (NULL == vesdeliver->crypto_handle) {
+    const char *dlerr = dlerror();
+    if (NULL == dlerr)
+        dlerr = "NULL";
+    GST_ERROR ("dlopen %s error: %s", crypto_lib_name, dlerr);
+    return;
+  }
+
+  vesdeliver->Content_Protection_Set_AppName =
+      dlsym (vesdeliver->crypto_handle, "Content_Protection_Set_AppName");
+  vesdeliver->Content_Protection_Copy_Init =
+      dlsym (vesdeliver->crypto_handle, "Content_Protection_Copy_Init");
+  vesdeliver->Content_Protection_Copy =
+      dlsym (vesdeliver->crypto_handle, "Content_Protection_Copy");
+  vesdeliver->Content_Protection_Copy_Terminate =
+      dlsym (vesdeliver->crypto_handle, "Content_Protection_Copy_Terminate");
+
+  if (!vesdeliver->Content_Protection_Set_AppName ||
+      !vesdeliver->Content_Protection_Copy_Init ||
+      !vesdeliver->Content_Protection_Copy ||
+      !vesdeliver->Content_Protection_Copy_Terminate) {
+    GST_ERROR ("dlsym failed with NULL symbol");
+    dlclose (vesdeliver->crypto_handle);
+    vesdeliver->crypto_handle = NULL;
+    return;
+  } else {
+    GST_INFO_OBJECT (vesdeliver, "open %s(%p) successfully",
+          crypto_lib_name, vesdeliver->crypto_handle);
+  }
+
+  int ret = SECURE_COPY_RETURN_SUCCESS;
+  ret = vesdeliver->Content_Protection_Copy_Init (&vesdeliver->secure_handle);
+  if (ret == SECURE_COPY_RETURN_SUCCESS) {
+    ret = vesdeliver->Content_Protection_Set_AppName (crypto_app_name);
+    if (ret != SECURE_COPY_RETURN_SUCCESS) {
+      GST_ERROR ("Content_Protection_Set_AppName failed with %d", ret);
+    }
+  } else {
+    GST_ERROR ("Content_Protection_Copy_Init failed with %d", ret);
+  }
+
+  gst_base_transform_set_in_place (GST_BASE_TRANSFORM (vesdeliver), FALSE);
 }
 
 static void
 gst_vesdeliver_finalize (GObject * object)
 {
   GstVesDeliver *vesdeliver = GST_VESDELIVER (object);
+
+  int ret = SECURE_COPY_RETURN_SUCCESS;
+  ret = vesdeliver->Content_Protection_Copy_Terminate (&vesdeliver->secure_handle);
+  if (ret != SECURE_COPY_RETURN_SUCCESS) {
+    GST_ERROR ("Content_Protection_Copy_Terminate failed with %d", ret);
+  }
+
+  if (vesdeliver->crypto_handle) {
+    GST_INFO_OBJECT (vesdeliver, "dlclose %s(%p)", crypto_lib_name, vesdeliver->crypto_handle);
+    dlclose (vesdeliver->crypto_handle);
+  }
+
   G_OBJECT_CLASS (parent_class)->finalize (object);
 }
 
-static GstStateChangeReturn
-gst_vesdeliver_change_state (GstElement * element, GstStateChange transition)
+static gboolean
+gst_vesdeliver_start (GstBaseTransform* trans)
 {
-  GstVesDeliver *vesdeliver = GST_VESDELIVER (element);
-  switch (transition) {
-    case GST_STATE_CHANGE_PAUSED_TO_READY: {
-      GST_INFO("STATE paused to ready");
-    }break;
-    default: {
-      break;
-    }
+  GstVesDeliver *vesdeliver = GST_VESDELIVER (trans);
+
+  if (NULL == vesdeliver->allocator) {
+    vesdeliver->allocator = gst_vesdeliver_allocator_new (vesdeliver->secure);
+    g_return_val_if_fail (vesdeliver->allocator != NULL, FALSE);
+    GST_DEBUG_OBJECT (vesdeliver, "Create vesdeliver allocator");
   }
-  return GST_ELEMENT_CLASS (parent_class)->change_state (element, transition);
+  return TRUE;
 }
 
 static gboolean
-gst_vesdeliver_decide_allocation (GstBaseTransform * trans, GstQuery * query)
+gst_vesdeliver_stop (GstBaseTransform* trans)
 {
   GstVesDeliver *vesdeliver = GST_VESDELIVER (trans);
-  GST_DEBUG_OBJECT(vesdeliver, "gst_vesdeliver_decide_allocation");
 
-  GstCaps *caps = NULL;
-  GstBufferPool *pool = NULL;
-  GstStructure *config = NULL;
-  GstAllocator *allocator = NULL;
-  guint size, minbuffers, maxbuffers;
-  GstAllocationParams params;
-  GstVideoInfo info;
-  gboolean update = FALSE;
-
-  gst_query_parse_allocation (query, &caps, NULL);
-
-  GST_DEBUG_OBJECT (vesdeliver, "allocation caps: %" GST_PTR_FORMAT, caps);
-  GST_DEBUG_OBJECT (vesdeliver, "allocation params: %" GST_PTR_FORMAT, query);
-
-  if (gst_query_get_n_allocation_params (query) > 0)
-    gst_query_parse_nth_allocation_param (query, 0, NULL, &params);
-
-  if (gst_query_get_n_allocation_pools (query) > 0) {
-    update = TRUE;
-    gst_query_parse_nth_allocation_pool (query, 0, &pool, &size, &minbuffers, &maxbuffers);
-    if (pool) {
-      GST_DEBUG_OBJECT (vesdeliver, "ignore buffer pool from downstream");
-      gst_object_unref (pool);
-      pool = NULL;
-    }
+  if (vesdeliver->allocator) {
+    gst_object_unref (vesdeliver->allocator);
+    vesdeliver->allocator = NULL;
+    GST_DEBUG_OBJECT (vesdeliver, "Unref vesdeliver allocator");
   }
 
-  if (vesdeliver->outpool) {
-    gst_object_unref (vesdeliver->outpool);
-  }
-
-  if (!gst_video_info_from_caps (&info, caps)) {
-    GST_ERROR_OBJECT (vesdeliver, "Invalid caps %" GST_PTR_FORMAT, caps);
-    return FALSE;
-  }
-  size = info.size;
-
-  // Create a new buffer pool.
-  pool = gst_vesdeliver_buffer_pool_new ();
-  minbuffers = DEFAULT_PROP_MIN_BUFFERS;
-  maxbuffers = DEFAULT_PROP_MAX_BUFFERS;
-
-  GST_DEBUG_OBJECT (vesdeliver, "allocation: size:%u min:%u max:%u pool:%"
-       GST_PTR_FORMAT, size, minbuffers, maxbuffers, pool);
-
-  config = gst_buffer_pool_get_config (pool);
-  gst_buffer_pool_config_set_params (config, caps, size, minbuffers, maxbuffers);
-
-  GST_DEBUG_OBJECT (vesdeliver, "setting pool config to %" GST_PTR_FORMAT, config);
-
-  if (!gst_buffer_pool_set_config (pool, config)) {
-    GST_WARNING_OBJECT (vesdeliver, "Failed to set pool configuration!");
-    g_object_unref (pool);
-    pool = NULL;
-  }
-
-  // Get the configured pool properties in order to set in query.
-  config = gst_buffer_pool_get_config (pool);
-  gst_buffer_pool_config_get_params (config, &caps, &size, &minbuffers,
-      &maxbuffers);
-
-  gst_structure_free (config);
-  config = NULL;
-
-  GST_DEBUG_OBJECT (vesdeliver, "setting pool with size: %d, min: %d, max: %d",
-                    size, minbuffers, maxbuffers);
-
-  /* update pool info in the query */
-  if (update) {
-    GST_DEBUG_OBJECT (vesdeliver, "update buffer pool");
-    gst_query_set_nth_allocation_pool (query, 0, pool, size, minbuffers, maxbuffers);
-  } else {
-    GST_DEBUG_OBJECT (vesdeliver, "new buffer pool");
-    gst_query_add_allocation_pool (query, pool, size, minbuffers, maxbuffers);
-  }
-
-   vesdeliver->outpool = pool;
-
-   return TRUE;
+  return TRUE;
 }
 
 static GstFlowReturn
@@ -203,19 +219,11 @@ gst_vesdeliver_prepare_output_buffer (GstBaseTransform * trans,
 {
   GstVesDeliver *vesdeliver = GST_VESDELIVER (trans);
 
-  *outbuf = gst_buffer_new_allocate(NULL, gst_buffer_get_size(inbuf), NULL);
+  *outbuf = gst_buffer_new_allocate (vesdeliver->allocator,
+                gst_buffer_get_size(inbuf), NULL);
 
-//  if (gst_buffer_pool_acquire_buffer (vesdeliver->outpool,
-//                                      outbuf, NULL) != GST_FLOW_OK) {
-//    GST_ERROR("Failed to acuqire buffer");
-//  }
-
-  GST_DEBUG_OBJECT(vesdeliver, "vesdeliver prepare_output_buffer size:%d",
-        gst_buffer_get_size(*outbuf));
-
-  g_return_val_if_fail(*outbuf != NULL, GST_FLOW_ERROR);
-
-  GST_BASE_TRANSFORM_CLASS(parent_class)->copy_metadata(trans, inbuf, *outbuf);
+  g_return_val_if_fail (*outbuf != NULL, GST_FLOW_ERROR);
+  GST_BASE_TRANSFORM_CLASS (parent_class)->copy_metadata (trans, inbuf, *outbuf);
 
   return GST_FLOW_OK;
 }
@@ -225,29 +233,88 @@ gst_vesdeliver_transform (GstBaseTransform * trans, GstBuffer * inbuf,
     GstBuffer * outbuf)
 {
   GstVesDeliver *vesdeliver = GST_VESDELIVER (trans);
+  GstMemory *out_mem = NULL;
+  int buf_fd = -1;
+  gsize fd_memory_size = 0;
 
   g_return_val_if_fail (gst_buffer_is_writable (outbuf), GST_FLOW_ERROR);
 
-  GstMapInfo map = {};
-  gst_buffer_map (inbuf, &map, GST_MAP_READ);
+  GstMapInfo input_map = {};
+  gst_buffer_map (inbuf, &input_map, GST_MAP_READ);
   GST_DEBUG_OBJECT (vesdeliver,
         "Input buffer %p (size %" G_GSIZE_FORMAT ", timestamp %" G_GUINT64_FORMAT
-        ", offset %" G_GUINT64_FORMAT "", inbuf, map.size,
+        ", offset %" G_GUINT64_FORMAT "", inbuf, input_map.size,
         GST_BUFFER_TIMESTAMP (inbuf), GST_BUFFER_OFFSET (inbuf));
 
-  GstMapInfo outBufferMap = {};
-  gst_buffer_map(outbuf, &outBufferMap, GST_MAP_READWRITE);
-  GST_DEBUG_OBJECT (vesdeliver,
-        "Output buffer %p (size %" G_GSIZE_FORMAT ", timestamp %" G_GUINT64_FORMAT
-        ", offset %" G_GUINT64_FORMAT "", outbuf, outBufferMap.size,
-        GST_BUFFER_TIMESTAMP (outbuf), GST_BUFFER_OFFSET (outbuf));
+  out_mem = gst_buffer_peek_memory (outbuf, 0);
+  if (gst_is_dmabuf_memory (out_mem)) {
+    buf_fd = gst_dmabuf_memory_get_fd (out_mem);
+    fd_memory_size = gst_memory_get_sizes (out_mem, NULL, NULL);
+  } else {
+    GST_ERROR ("Invalide gst buffer type");
+    return GST_FLOW_ERROR;
+  }
 
-  memcpy(outBufferMap.data, map.data, outBufferMap.size);
+  if (vesdeliver->secure) {
+    uint32_t bytes_copied = 0;
+    gsize input_size = gst_buffer_get_size (inbuf);
+    int ret = SECURE_COPY_RETURN_SUCCESS;
 
-  gst_buffer_unmap (inbuf, &map);
-  gst_buffer_unmap (outbuf, &outBufferMap);
+    ret = vesdeliver->Content_Protection_Copy (vesdeliver->secure_handle, input_map.data,
+              input_size, buf_fd, 0, &bytes_copied, SECURE_COPY_NONSECURE_TO_SECURE);
+    if (ret == SECURE_COPY_RETURN_SUCCESS) {
+      GST_DEBUG_OBJECT (vesdeliver, "secure copy input size: %u, sec buf_fd: %d, bytes copied: %u",
+          input_size, buf_fd, bytes_copied);
+    } else {
+      GST_ERROR ("Content_Protection_Copy failed with %d", ret);
+    }
+  } else {
+    void *ptr = NULL;
+    ptr = mmap (NULL, fd_memory_size, PROT_READ | PROT_WRITE, MAP_SHARED, buf_fd, 0);
+    if (ptr) {
+      memcpy (ptr, input_map.data, input_map.size);
+      GST_DEBUG_OBJECT (vesdeliver, "memcpy to %p with buf_fd: %d, size: %d,",
+              ptr, buf_fd, fd_memory_size);
+      munmap (ptr, fd_memory_size);
+    }
+  }
+
+  gst_buffer_unmap (inbuf, &input_map);
 
   return GST_FLOW_OK;
+}
+
+static void
+gst_vesdeliver_set_property(GObject* object, guint property_id,
+    const GValue* value, GParamSpec* pspec)
+{
+  GstVesDeliver* self = GST_VESDELIVER(object);
+
+  switch (property_id) {
+    case PROP_SECURE:
+      self->secure = g_value_get_boolean (value);
+      GST_DEBUG_OBJECT (self, "secure mode: %s", self->secure ? "TRUE" : "FALSE");
+      break;
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID(object, property_id, pspec);
+      break;
+  }
+}
+
+static void
+gst_vesdeliver_get_property(GObject* object, guint property_id,
+    GValue* value, GParamSpec* pspec)
+{
+  GstVesDeliver* self = GST_VESDELIVER (object);
+
+  switch (property_id) {
+    case PROP_SECURE:
+      g_value_set_boolean (value, self->secure);
+      break;
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+      break;
+  }
 }
 
 /* entry point to initialize the plug-in
--- gst-plugin-vesdeliver/gstvesdeliverpool.c
+++ gst-plugin-vesdeliver/gstvesdeliverpool.c
@@ -6,72 +6,129 @@
 #include <linux/msm_ion.h>
 #include "gstvesdeliverpool.h"
 
-GST_DEBUG_CATEGORY_STATIC (gst_vesdeliver_pool_debug);
-#define GST_CAT_DEFAULT gst_vesdeliver_pool_debug
-
-#define gst_vesdeliver_buffer_pool_parent_class parent_class
-G_DEFINE_TYPE (GstVesDeliverBufferPool, gst_vesdeliver_buffer_pool,
-    GST_TYPE_BUFFER_POOL);
-
-/* Dynamically load libion by dlopen. */
+/* Dynamically load libs by dlopen. */
 static const char *ion_lib_name  = "libion.so.0";
-static void* ion_lib = NULL;
+
+GST_DEBUG_CATEGORY_EXTERN (vesdeliver_debug);
+#define GST_CAT_DEFAULT vesdeliver_debug
+
+#define gst_vesdeliver_allocator_parent_class parent_class
+G_DEFINE_TYPE (GstVesDeliverAllocator, gst_vesdeliver_allocator,
+    GST_TYPE_DMABUF_ALLOCATOR);
+
+#define ALIGN(num, to) (((num) + (to - 1)) & (~(to - 1)))
 
 static void
-gst_vesdeliver_buffer_pool_init (GstVesDeliverBufferPool * pool)
+gst_vesdeliver_allocator_init (GstVesDeliverAllocator *alloc)
 {
-  GST_DEBUG_CATEGORY_INIT (gst_vesdeliver_pool_debug,
-      "vesdeliverpool", 0, "QTI GST vesdeliver buffer pool");
-  pool->allocator = NULL;
+  GstAllocator *allocator = GST_ALLOCATOR_CAST (alloc);
+  allocator->mem_type = GST_ALLOCATOR_VESDELIVER;
+  GST_OBJECT_FLAG_SET (alloc, GST_ALLOCATOR_FLAG_CUSTOM_ALLOC);
 
-  if (NULL == ion_lib) {
-    ion_lib = dlopen (ion_lib_name, RTLD_NOW);
-    GST_INFO_OBJECT (pool, "open ION lib: %p", ion_lib);
-    if (ion_lib == NULL) {
-      GST_ERROR ("dlopen libion.so failed");
-      return;
-    }
+  alloc->ion_handle = dlopen (ion_lib_name, RTLD_NOW);
+  if (NULL == alloc->ion_handle) {
+    const char *dlerr = dlerror();
+    if (NULL == dlerr)
+        dlerr = "NULL";
+    GST_ERROR ("dlopen %s error: %s", ion_lib_name, dlerr);
+    return;
+  }
+
+  alloc->ion_open = dlsym (alloc->ion_handle, "ion_open");
+  alloc->ion_close = dlsym (alloc->ion_handle, "ion_close");
+  alloc->ion_alloc_fd = dlsym (alloc->ion_handle, "ion_alloc_fd");
+
+  if (!alloc->ion_open || !alloc->ion_close || !alloc->ion_alloc_fd) {
+    GST_ERROR ("dlsym failed with ion_open: %p, ion_close: %p, ion_alloc_fd: %p",
+          alloc->ion_open, alloc->ion_close, alloc->ion_alloc_fd);
+    dlclose (alloc->ion_handle);
+    alloc->ion_handle = NULL;
+    return;
+  } else {
+    GST_INFO_OBJECT (alloc, "open %s(%p) successfully", ion_lib_name, alloc->ion_handle);
+  }
+
+  alloc->ion_fd = alloc->ion_open();
+  if (alloc->ion_fd < 0) {
+    GST_ERROR ("Open ION device failed with %d", alloc->ion_fd);
+    return;
   }
 }
 
-static void
-gst_vesdeliver_buffer_pool_finalize (GObject * object)
+GstAllocator *
+gst_vesdeliver_allocator_new (gboolean secure)
 {
-  GstVesDeliverBufferPool *pool = GST_VESDELIVER_BUFFER_POOL (object);
+  GstVesDeliverAllocator *allocator = NULL;
 
-  GST_INFO_OBJECT (pool, "pool %p", pool);
-  if (pool->allocator)
-    gst_object_unref (pool->allocator);
+  allocator = (GstVesDeliverAllocator *) g_object_new (GST_TYPE_VESDELIVER_ALLOCATOR, NULL);
+  if (NULL == allocator) {
+    GST_ERROR ("Failed to create vesdeliver allocator");
+    return NULL;
+  }
+  allocator->secure = secure;
 
-  if (ion_lib) {
-    dlclose (ion_lib);
-    ion_lib = NULL;
+  GST_INFO_OBJECT (allocator, "Create vesdeliver %s allocator %p",
+    allocator->secure ? "secure":"normal", allocator);
+
+  return GST_ALLOCATOR_CAST (allocator);
+}
+
+void
+gst_vesdeliver_allocator_finalize (GObject *object)
+{
+  GstVesDeliverAllocator* allocator = GST_VESDELIVER_ALLOCATOR_CAST(object);
+  if (allocator->ion_fd > 0) {
+    allocator->ion_close (allocator->ion_fd);
+    allocator->ion_fd = -1;
+  }
+
+  if (allocator->ion_handle) {
+    GST_INFO_OBJECT (allocator, "dlclose %s(%p)", ion_lib_name, allocator->ion_handle);
+    dlclose (allocator->ion_handle);
   }
 
   G_OBJECT_CLASS (parent_class)->finalize (object);
 }
 
-static void
-gst_vesdeliver_buffer_pool_class_init (GstVesDeliverBufferPoolClass * klass)
+static GstMemory *
+gst_vesdeliver_allocator_alloc (GstAllocator *allocator, gsize size,
+    GstAllocationParams *params)
 {
-  GObjectClass *gobj_class = (GObjectClass *) klass;
-  GstBufferPoolClass *bp_class = (GstBufferPoolClass *) klass;
+  GstVesDeliverAllocator *alloc = GST_VESDELIVER_ALLOCATOR (allocator);
+  int rc = -EINVAL;
+  GstMemory *mem = NULL;
+  guint flags = 0;
+  guint heap_mask = ION_HEAP (ION_SYSTEM_HEAP_ID);
+  gint buf_fd = -1;
+  gsize alloc_size = ALIGN (size, 4096);
 
-  gobj_class->finalize = GST_DEBUG_FUNCPTR (gst_vesdeliver_buffer_pool_finalize);
-}
+  if (alloc->secure) {
+    flags = ION_FLAG_SECURE | ION_FLAG_CP_BITSTREAM;
+    heap_mask = ION_HEAP (ION_SECURE_HEAP_ID) | ION_HEAP (ION_SECURE_DISPLAY_HEAP_ID);
+  }
 
-GstBufferPool *
-gst_vesdeliver_buffer_pool_new ()
-{
-  GstVesDeliverBufferPool *pool = NULL;
+  rc = alloc->ion_alloc_fd (alloc->ion_fd, alloc_size, 0, heap_mask, flags, &buf_fd);
 
-  pool = (GstVesDeliverBufferPool*)
-      g_object_new (GST_TYPE_VESDELIVER_BUFFER_POOL, NULL);
-
-  if (!pool) {
-    GST_ERROR ("failed to create buffer pool");
+  if (rc || buf_fd < 0) {
+    GST_ERROR ("ion_alloc_fd failed with rc = %d", rc);
     return NULL;
   }
 
-  return GST_BUFFER_POOL (pool);
+  mem = gst_dmabuf_allocator_alloc (allocator, buf_fd, alloc_size);
+  if (mem) {
+    GST_INFO_OBJECT (alloc, "Allocate %s gstmemory with size = %" G_GSIZE_FORMAT ", fd = %d",
+        alloc->secure ? "secure" : "normal", alloc_size, buf_fd);
+  }
+
+  return mem;
+}
+
+static void
+gst_vesdeliver_allocator_class_init (GstVesDeliverAllocatorClass *klass)
+{
+  GObjectClass *obj_class = G_OBJECT_CLASS (klass);
+  GstAllocatorClass *allocator_class = GST_ALLOCATOR_CLASS (klass);
+
+  obj_class->finalize = GST_DEBUG_FUNCPTR (gst_vesdeliver_allocator_finalize);
+  allocator_class->alloc = GST_DEBUG_FUNCPTR (gst_vesdeliver_allocator_alloc);
 }
