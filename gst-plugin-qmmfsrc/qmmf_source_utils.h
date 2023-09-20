50d5dadd11a15b476bf6a4ccbffdf00c41245777
Gautam Naidu Bodala <quic_gbodala@quicinc.com>
2023-09-20 03:59:49 -0700

    Revert "gst-plugin-qmmfsrc:support camera opration mode property"
    This reverts commit aaf9173a1984bcd52d922cc671a3612f9635e45e.
    
    Reason for revert: Need rework
    
    Change-Id: Ic0b175afd1370193fd4c12514dfe876bd4169c7d

--- gst-plugin-qmmfsrc/qmmf_source_utils.h
+++ gst-plugin-qmmfsrc/qmmf_source_utils.h
@@ -136,7 +136,6 @@ G_BEGIN_DECLS
 #define GST_TYPE_QMMFSRC_CAPTURE_MODE (gst_qmmfsrc_capture_mode_get_type())
 #define GST_TYPE_QMMFSRC_FRC_MODE (gst_qmmfsrc_frc_mode_get_type())
 #define GST_TYPE_QMMFSRC_ROTATE (gst_qmmfsrc_rotate_get_type())
-#define GST_TYPE_QMMFSRC_CAM_OPMODE (gst_qmmfsrc_cam_opmode_get_type())
 
 #define GST_BAYER_FORMAT_OFFSET 0x1000
 
@@ -289,11 +288,6 @@ enum
   ROTATE_270CCW,
 };
 
-typedef enum {
-  CAM_OPMODE_NONE,
-  CAM_OPMODE_FRAMESELECTION,
-} GstCamOpMode;
-
 GType gst_qmmfsrc_control_mode_get_type (void);
 
 GType gst_qmmfsrc_effect_mode_get_type (void);
@@ -321,8 +315,6 @@ GType gst_qmmfsrc_capture_mode_get_type (void);
 GType gst_qmmfsrc_frc_mode_get_type (void);
 
 GType gst_qmmfsrc_rotate_get_type(void);
-
-GType gst_qmmfsrc_cam_opmode_get_type (void);
 
 guchar gst_qmmfsrc_control_mode_android_value (const guint value);
 
