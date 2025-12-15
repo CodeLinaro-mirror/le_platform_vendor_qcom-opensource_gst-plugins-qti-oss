f3de6a697fb72035bed1aa009e6b41295c4bd40d
Hongyong Huang <hongyong@qti.qualcomm.com>
2025-12-15 16:18:31 +0800

    mltflite: Add support for tflite master branch compilation
    - tensorflow/lite/version.h header in master branch tflite will not pass
      CMake check_include_file check. That check is used to build plugin
      against master branch headers.
    - Propagate from 6167659 in mainline.
    
    Change-Id: Idcd9312de8c61a62368b57847b7937fd0dea63c8

--- gst-plugin-mltflite/ml-tflite-engine.h
+++ gst-plugin-mltflite/ml-tflite-engine.h
@@ -68,7 +68,9 @@
 #include <gst/allocators/allocators.h>
 #include <gst/ml/ml-info.h>
 #include <gst/ml/ml-frame.h>
+#if defined(HAVE_TFLITE_VERSION_H)
 #include <tensorflow/lite/version.h>
+#endif //HAVE_TFLITE_VERSION_H
 
 G_BEGIN_DECLS
 
