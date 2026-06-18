qualcomm/qimsdk

codec2: enable Hierarchical B-frames mechanism
Support to enable Hierarchical B-frames mechanism.

Change-Id: Ice23f1fb9a0dfea7ce5d1491ae84c17b0cc9c549
Signed-off-by: Hui Liu <huliu@qti.qualcomm.com>

gst-plugin-codec2/c2venc/c2venc.c
@@ -763,6 +763,14 @@ gst_c2_venc_setup_parameters (GstC2VEncoder * c2venc,
          GST_ERROR_OBJECT (c2venc, "Failed to enable heir bpreconditions!");
          return FALSE;
        }
+
+       success = gst_c2_engine_set_parameter (c2venc->engine,
+           GST_C2_PARAM_NATIVE_RECORDING, GST_PTR_CAST (&enable));
+       if (!success) {
+         GST_ERROR_OBJECT (c2venc, "Failed to enable heir bpreconditions"
+             " or native recording!");
+         return FALSE;
+       }
     } else if (c2venc->temp_layer.n_layers == 0 &&
         c2venc->temp_layer.n_blayers == 0) {
       enable = FALSE;

CodeLinaro-mirror/le_platform_vendor_qcom-opensource_gst-plugins-qti-oss

gst-plugin-codec2/c2venc/c2venc.c
@@ -668,6 +668,14 @@ gst_c2_venc_setup_parameters (GstC2VEncoder * c2venc,
          GST_ERROR_OBJECT (c2venc, "Failed to enable heir bpreconditions!");
          return FALSE;
        }
+
+       success = gst_c2_engine_set_parameter (c2venc->engine,
+           GST_C2_PARAM_NATIVE_RECORDING, GPOINTER_CAST (&enable));
+       if (!success) {
+         GST_ERROR_OBJECT (c2venc, "Failed to enable heir bpreconditions"
+             " or native recording!");
+         return FALSE;
+       }
     } else if (c2venc->temp_layer.n_layers == 0 &&
         c2venc->temp_layer.n_blayers == 0) {
       enable = FALSE;